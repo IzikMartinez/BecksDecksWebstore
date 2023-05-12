9a6a412b24b001323e5d65c34e93ede70af3812e
Isaac Martinez <IsaacR454@gmail.com>
2023-05-12 14:30:45 -0500

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,106 @@
 import Image from 'next/image'
+import styles from "app/styles/home.module.css"
+
+interface BubbleProps {
+  itemName: string, 
+  itemPrice: number, 
+  description: string
+}
+
+interface IconProps {
+  path: string,
+  width: number,
+  height: number
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+  <body className={styles.home}>
+    <div>
+      <Splash></Splash>
+    </div>
+    <div className='fixed flex flex-row w-screen h-screen top-24'>
+      <div>
+        <SideBar />
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='fixed flex flex-wrap flex-grow left-24 top-8 h-screen items-center justify-center'>
+        <Bubble itemName="test" itemPrice={100.00} description='lorem ipso factum'></Bubble>
+        <Bubble itemName="Sponge" itemPrice={100.00} description='lorem ipso factum'></Bubble>
+        <Bubble itemName="Bob" itemPrice={100.00} description='lorem ipso factum'></Bubble>
+        <Bubble itemName="Lookin" itemPrice={100.00} description='lorem ipso factum'></Bubble>
+        <Bubble itemName="like" itemPrice={100.00} description='lorem ipso factum'></Bubble>
+        <Bubble itemName="a snack" itemPrice={100.00} description='lorem ipso factum'></Bubble>     
+        <Bubble itemName="test" itemPrice={100.00} description='lorem ipso factum'></Bubble>
+        <Bubble itemName="test" itemPrice={100.00} description='lorem ipso factum'></Bubble>
+        <Bubble itemName="test" itemPrice={100.00} description='lorem ipso factum'></Bubble>
+        <Bubble itemName="sponge" itemPrice={100.00} description='lorem ipso factum'></Bubble>
       </div>
+    </div>
+  </body>
+)}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+function Splash() {
+  return (
+  <span className='flex fixed w-screen h-24 top-0 left-0 bg-gradient-to-r from-pastel-coral via-pastel-yellow to-yellow-compliment text-black  shadow-xl shadow-blue-gray-800 text-center items-center justify-center'> 
+    <div className={styles.title}>
+      SPARKLING CITY LGS
+    </div>
+  <BarIcon path="cart.svg" width={180} height={180}></BarIcon>
+  </span>
+  )
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+function SideBar() {
+  const items = [{id: 0, name: "Flesh and Blood"}, {id: 1, name: "Warhammer"}, {id: 2, name: "Magic"}]
+  return (
+    <span className={styles.sidebar}>
+      {items.map(item => (
+        <SidebarItem key={item.id} name={item.name} ></SidebarItem>
+      ))}
+    </span>
+)}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+interface sidebarItemProps {
+  name: string
+}
+function SidebarItem(props: sidebarItemProps) {
+  return (
+  <span className={styles.sidebarItem}>
+    {props.name}
+  </span>
+  )
+}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+function BarIcon(props: IconProps) {
+  return (
+    <span className='w-20 h-20 rounded-lg mr-6'>
+      <Image src={props.path} alt='hold' width={props.width} height={props.height} />
+    </span>
+  )}
+
+function Bubble(props: BubbleProps) {
+  return (
+    <div className='my-0'>
+    <span className='flex flex-col w-64 h-56 items-center justify-center text-black rounded-lg mx-10 shadow-xl shadow-blue-gray-800'>
+      <div className='flex flex-col flex-grow w-64 h-32 items-center justify-center bg-wave-blue-200 rounded-t-xl'>
+        <img src="placeholder.jpg" alt="spon" className="w-48 h-36 text-center rounded-lg"/>
       </div>
-    </main>
+      <div className='flex w-64 h-7 bg-blue-400 text-white items-center justify-center text-2xl font-texgyre-adventor small-caps font-semibold'>
+          {props.itemName}  <br />
+      </div>
+      <div className='flex w-64 h-8 bg-blue-700 text-white items-center justify-center text-xl font-texgyre-adventor font-bold'>
+        ${props.itemPrice}
+      </div>
+    </span>
+    <BubbleBtn/>
+    </div>
+  )
+}
+
+function BubbleBtn() {
+  return (
+    <span className={styles.addToCart}>
+      ADD TO CART
+    </span>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,26 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'wave-blue-100': '#c8fefe',
+        'wave-blue-200': '#a8eeee',
+        'wave-blue-400': '#96fdfd',
+        'wave-compliment': '#96cafd',
+        'dark-wave': '#6098b7',
+        'pastel-yellow': '#fdfd96',
+        'yellow-compliment': '#fdca96',
+        'pastel-coral': '#fd9696'
+      },
+      fontFamily: {
+        'title-display': ['Bangers', 'cursive'],
+        'main-display': ['Yeseva', 'cursive'],
+        'sans-fira': ['Fira Sans', 'sans-serif'],
+        'texgyre-adventor': ['Tex Gyre Heros', 'sans-serif']
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('tailwindcss-opentype')
+  ],
 }
