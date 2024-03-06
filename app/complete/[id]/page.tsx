0446c019fecfc8c3cbcde0bf554339b276e970ac
Isaac Martinez <IsaacR454@gmail.com>
2024-03-06 06:30:05 -0600

    update mobile UI.
    More pages updated

--- app/complete/[id]/page.tsx
+++ app/complete/[id]/page.tsx
@@ -9,7 +9,7 @@ export default function Page({params}: { params: {id: number} }) {
     }, [router])
   return (
       <div className={`flex h-screen w-screen items-center justify-center bg-cover bg-[url('waves.svg')] `}>
-        <div className='flex flex-col w-1/4 h-1/3 rounded-md bg-slate-800 items-center justify-between text-center'>
+        <div className='flex flex-col lg:w-1/4 lg:h-1/3 w-10/12 rounded-md bg-slate-800 items-center justify-between text-center'>
             <div className='text-white text-2xl my-6'>Payment Complete</div>
             <div className='text-white text-xl lg:px-8 px-4'>{`Thank you for your payment. Your order number is #${params.id}`}</div>
             <div className='text-wave-blue-100 text-md mt-8 cursor-pointer my-4' onClick={navigateHome}>Return Home</div>
