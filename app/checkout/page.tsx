9a0c24d4ea760ffbf26db933d63437f7b7873366
Isaac Martinez <IsaacR454@gmail.com>
2024-03-07 18:16:39 -0600

    Remove component for handling mobile checkout components
    Exports will throw an error when building due to how react expects components to behave

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,12 +1,11 @@
 'use client';
-import {useEffect, useState} from 'react';
+import {createContext, useContext, useEffect, useState} from 'react';
 import { useAppSelector } from '../hooks';
 import { selectQuantity, selectTotalCartPrice } from '../GlobalRedux/cartSlice';
 import {PaymentWindow, ShippingOptions} from "@/app/components/paymentForms";
 import {FormValidityContext} from "@/app/context";
 import useMobileDetect from "@/app/utils/useMobileDetect";
 import checkAndSplit from "@/app/utils/splitStrings";
-import MobileMenu from "@/app/components/checkoutMenus/checkoutMenu";
 
 const TAX = 0.0825
 interface CartElementProps {
@@ -100,4 +99,78 @@ export function CartTotal({total, text}: cartTotalProps) {
   return (
     <span className='flex text-2xl font-bold text-gray-600 my-2 items-center justify-center'>{text}: ${total.toFixed(2)}</span>
   )
+}
+
+
+
+const CheckoutMenuContext = createContext({
+    isSwiped: false,
+    setIsSwiped: (value: boolean) => {}
+})
+
+function MobileMenu() {
+    const [isSwiped, setIsSwiped] = useState(false)
+    return(
+        <CheckoutMenuContext.Provider value={{isSwiped, setIsSwiped}}>
+            <div>
+                { isSwiped ? <MobileMenuPayment/> : <MobileMenuShipping/> }
+            </div>
+        </CheckoutMenuContext.Provider>
+    )
+}
+
+function MobileMenuShipping() {
+    const cartStore = useAppSelector(state => state.cartItems)
+    const total = useAppSelector(selectTotalCartPrice)
+    const {isSwiped, setIsSwiped} = useContext(CheckoutMenuContext)
+    const handleSwipe = ()=> {
+        setIsSwiped(!isSwiped)
+    }
+    let shippingStyle =  isSwiped?
+        "flex flex-col w-80 h-[38rem] bg-gradient-to-b from-blue-500 to-red-100 rounded-lg transform -translate-x-96 transition-transform duration-300"
+        : "flex flex-col w-80 h-[38rem] bg-gradient-to-b from-blue-500 to-red-100 rounded-lg transform translate-x-0 transition-transform duration-300"
+    return(
+        <div className={`${shippingStyle}`}>
+            <div className='flex items-start justify-center lg:my-2 mt-8 flex-grow'>
+                {cartStore.cartItems.map((cartItem) => (
+                    <div key={cartItem.name}>
+                        <CartElement id={cartItem.id} name={cartItem.name} price={cartItem.price}
+                                     quantity={0}/>
+                    </div>
+                ))}
+            </div>
+            <ShippingOptions/>
+            <CartTotal total={total * TAX} text="Tax"/>
+            <div className=''>
+                <CartTotal total={total * (1 + TAX)} text="Total"/>
+            </div>
+            <button
+                className={'h-12 rounded-b-lg bg-gradient-radial from-green-500 to-green-700 font-bold text-xl'}
+                onClick={handleSwipe}
+            >Next
+            </button>
+        </div>
+    )
+}
+
+function MobileMenuPayment() {
+    const {isSwiped, setIsSwiped} = useContext(CheckoutMenuContext)
+    const handleSwipe = ()=> {
+        setIsSwiped(!isSwiped)
+    }
+    let shippingStyle =  isSwiped?
+        "flex flex-col w-80 h-[38rem] bg-gradient-to-b from-blue-500 to-red-100 rounded-lg transform translate-x-0 transition-transform duration-300"
+        : "flex flex-col w-80 h-[38rem] bg-gradient-to-b from-blue-500 to-red-100 rounded-lg transform translate-x-96 transition-transform duration-300"
+    return (
+        <div className={`${shippingStyle}`}>
+            <div className='flex flex-col items-center justify-center lg:my-2 mt-8 flex-grow'>
+                <button
+                    className={'absolute h-12 w-full top-0 rounded-t-lg bg-gradient-radial from-purple-400 to-purple-700 font-bold text-xl'}
+                    onClick={handleSwipe}
+                >Back
+                </button>
+                <PaymentWindow/>
+            </div>
+        </div>
+    )
 }
